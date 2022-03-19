381d8cdc7628c907c0c8c35bd8b448a9dcf95799
David <Dvillegas0365@outlook.com>
2022-03-19 13:01:00 -0400

    Add some color Sensor code

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,9 +8,18 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
 
 public class ShooterSubsystem extends SubsystemBase {
     public final  CANSparkMax m_intakeFront = new CANSparkMax(ShooterConstants.intake, MotorType.kBrushed);
@@ -18,15 +27,22 @@ public class ShooterSubsystem extends SubsystemBase {
     private final CANSparkMax m_shooterEnd = new CANSparkMax(ShooterConstants.shooter, MotorType.kBrushless);    
   
     private final RelativeEncoder m_shooterEndEncoder = m_shooterEnd.getEncoder();
-     private SparkMaxPIDController m_shooterPID = m_shooterEnd.getPIDController();
-
-     
+    private SparkMaxPIDController m_shooterPID = m_shooterEnd.getPIDController();
+    
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    Color detectedColor = m_colorSensor.getColor();
     
      private boolean isBallPrimed = false;
     private boolean onTarget = false;
+
+
+  private final Color kBlueTarget = new Color(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
+  private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
     
     public void shooterSubsystem() {
-
 
     }
 
@@ -34,6 +50,9 @@ public class ShooterSubsystem extends SubsystemBase {
     public void periodic() {
       
       SmartDashboard.putNumber("Actual Motor RPM", m_shooterEndEncoder.getVelocity());
+      SmartDashboard.putNumber("Shooter Motor Temp", m_shooterEnd.getMotorTemperature());
+      
+   
      
 }
 
@@ -43,13 +62,13 @@ public class ShooterSubsystem extends SubsystemBase {
     m_intakeFront.set(ShooterConstants.intakePower);
     m_conveyorMiddle.set(ShooterConstants.conveyorLowPower);
     
-    /*
+    
     if(limitSwitch.get()){
       m_conveyorMiddle.set(0);
       isBallPrimed = true;
       Return;
     } else {
-      m_conveyorMiddle.set(ShooterConstants.conveyorlowPower);
+      m_conveyorMiddle.set(ShooterConstants.conveyorLowPower);
       if(!limitSwitch.get()){
         m_conveyorMiddle.set(0);
         isBallPrimed = false;
@@ -57,7 +76,7 @@ public class ShooterSubsystem extends SubsystemBase {
       } 
 
     }
-    */
+    
   }
 
     // Method to to reverse Intake and eject balls
@@ -78,8 +97,6 @@ public class ShooterSubsystem extends SubsystemBase {
     public void m_conveyorMiddleOn (){
       m_conveyorMiddle.set(ShooterConstants.conveyorLowPower);
     }
-
-
 
       ///////////////    Shooter Command   //////////////
 
