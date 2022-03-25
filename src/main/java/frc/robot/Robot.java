e39e1b096a2cf32b5d9cb07a90e79bca24f9d441
jafillmore <fillmore.joel@gmail.com>
2022-03-25 10:38:17 -0400

    added rio cameras

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,7 +29,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    // UsbCamera camera = CameraServer.startAutomaticCapture();
+    UsbCamera camera1 = CameraServer.startAutomaticCapture();
+    UsbCamera camera2 = CameraServer.startAutomaticCapture();
     m_robotContainer = new RobotContainer();
   }
 
