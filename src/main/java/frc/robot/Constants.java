b884d66e1f51378d4d5fb841b700bc3a2f478c4d
jafillmore <fillmore.joel@gmail.com>
2022-03-29 12:04:56 -0400

    fixed drive encoder inversions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -121,8 +121,8 @@ public final class Constants {
     public static final int[] kRearRightEncoderPorts = new int[] {6, 7};
 
     public static final boolean kFrontLeftEncoderReversed = false;
-    public static final boolean kRearLeftEncoderReversed = true;
-    public static final boolean kFrontRightEncoderReversed = false;
+    public static final boolean kRearLeftEncoderReversed = false;
+    public static final boolean kFrontRightEncoderReversed = true;
     public static final boolean kRearRightEncoderReversed = true;
 
     public static final double kTrackWidth = 0.5;
